f6ca6fc1f0ecef43fcf45ff15ef75d75b607afb1
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-03 22:50:10 +0200

    created NavBar.js, that renders navigation links

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav>
+            <a href="">Start</a>
+            <a href="">O co chodzi?</a>
+            <a href="">O nas</a>
+            <a href="">Fundacja i organizacje</a>
+            <a href="">Kontakt</a>
+        </nav>
+    );
+}
+
+export default NavBar;
