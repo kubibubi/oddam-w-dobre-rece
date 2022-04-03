2fa5a26a4d28055c505e6e78458b7075491a348a
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-03 22:55:48 +0200

    added NavBar and HomeBanner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import LoginBar from "./LoginBar";
+import LoginBar from "../LoginBar";
+import NavBar from "../NavBar";
+import HomeBanner from "./HomeBanner";
 
 function Home() {
     return (
-        <LoginBar />
+        <>
+            <LoginBar />
+            <NavBar />
+            <HomeBanner />
+        </>
     );
 }
+
 export default Home;
