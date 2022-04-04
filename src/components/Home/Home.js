980b1d4e20e9dd986161ceb1555b9cbe1e0a3f37
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 11:31:40 +0200

    refactored code to render some elements inside HomeBanner.js instead of Home.js to make things easier to style properly. Added Decoration element to HomeBanner.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import LoginBar from "../LoginBar";
-import NavBar from "../NavBar";
+
 import HomeBanner from "./HomeBanner";
 
 function Home() {
     return (
         <>
-            <LoginBar />
-            <NavBar />
             <HomeBanner />
         </>
     );
--- src/components/Home/HomeBanner.js
+++ src/components/Home/HomeBanner.js
@@ -1,15 +1,22 @@
 import React from 'react';
+
+import LoginBar from "../LoginBar";
+import NavBar from "../NavBar";
 import ButtonAction from '../ButtonAction';
+import Decoration from '../Decoration';
 
 function HomeBanner() {
     return (
         <>
-        <div className="home__banner">
-            <h2>Zacznij pomagać!</h2>
-            <h2>Oddaj niechciane rzeczy w zaufane ręce</h2>
-            <ButtonAction btnText='ODDAJ RZECZY' />
-            <ButtonAction btnText='ZORGANIZUJ ZBIÓRKĘ' />
-        </div>
+            <LoginBar/>
+            <NavBar/>
+            <div className="home__banner">
+                <h3>Zacznij pomagać!</h3>
+                <h3>Oddaj niechciane rzeczy w zaufane ręce</h3>
+                <Decoration/>
+                <ButtonAction btnText='ODDAJ RZECZY'/>
+                <ButtonAction btnText='ZORGANIZUJ ZBIÓRKĘ'/>
+            </div>
         </>
     );
 }
