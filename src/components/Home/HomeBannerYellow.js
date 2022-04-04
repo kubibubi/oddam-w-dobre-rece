cdc3a0bb80d7531555069215313ab9ddcc504894
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 12:25:25 +0200

    refactored className to keep the naming and for future styles

--- src/components/Home/HomeBannerYellow.js
+++ src/components/Home/HomeBannerYellow.js
@@ -3,17 +3,17 @@ import React from 'react';
 function HomeBannerYellow({ collectedBags, supportedOrganizations, organizedCollections }) {
     return (
         <section className="home__banner__yellow">
-            <div className="banner__info">
+            <div className="banner__yellow__info">
                 <h2>{collectedBags}</h2>
                 <h4>oddanych worków</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-            <div className="banner__info">
+            <div className="banner__yellow__info">
                 <h2>{supportedOrganizations}</h2>
                 <h4>wspartych organizacji</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
             </div>
-            <div className="banner__info">
+            <div className="banner__yellow__info">
                 <h2>{organizedCollections}</h2>
                 <h4>zorganizowanych zbiórek</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
