40936232023106fb5c87ca14b0e3fce2b043a7f1
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 12:13:59 +0200

    created HomeBannerGuide.js and added imports to assets

---
+++ src/components/Home/HomeBannerGuide.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import IconShirt from '../../assets/Icon-1.png'
+import IconBag from '../../assets/Icon-2.png'
+import IconGlass from '../../assets/Icon-3.png'
+import IconUroboros from '../../assets/Icon-4.png'
+
+function HomeBannerGuide(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default HomeBannerGuide;
