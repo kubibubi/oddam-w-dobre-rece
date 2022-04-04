ec3d4846b6c139048125061efc2d810d3cf0fa93
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 18:58:47 +0200

    Created Register element

---
+++ src/components/Register/Register.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import LoginBar from '../LoginBar';
+import NavBar from '../NavBar';
+import Decoration from '../Decoration';
+
+function Register() {
+    return (
+        <>
+            <LoginBar/>
+            <NavBar/>
+            <section className="register__section">
+                <h3>Zaloguj się</h3>
+                <Decoration/>
+                <div className="register__section__wrapper">
+                    <h4>Email</h4>
+                    <input type="text"/>
+                    <h4>Hasło</h4>
+                    <input type="password"/>
+                    <h4>Powtórz hasło</h4>
+                    <input type="password"/>
+                </div>
+                <LoginBar/>
+            </section>
+        </>
+    );
+}
+
+export default Register;
