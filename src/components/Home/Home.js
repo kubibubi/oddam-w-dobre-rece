eb94597c7e5e96590be6f8eec8a063d24d67b5ce
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 12:01:01 +0200

    added HomeBannerYellow.js import and rendering with placeholder props.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import HomeBanner from "./HomeBanner";
+import HomeBannerYellow from "./HomeBannerYellow";
 
 function Home() {
     return (
         <>
             <HomeBanner />
+            <HomeBannerYellow collectedBags={10}
+                              supportedOrganizations={5}
+                              organizedCollections={7} />
         </>
     );
 }
