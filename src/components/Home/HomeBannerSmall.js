d196dced42e899ba248ec04adb40e9bb08644967
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 12:02:17 +0200

    created HomeBannerSmall.js

---
+++ src/components/Home/HomeBannerSmall.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import DecorationImage from '../assets/Decoration.svg'
+
+function HomeBannerSmall() {
+    return (
+        <>
+            <h3>Wystarczą 4 proste kroki</h3>
+            <DecorationImage />
+        </>
+    );
+}
+
+export default HomeBannerSmall;
