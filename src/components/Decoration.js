4ac21938a90cd7fbab3595ec53c1d7fc9d885b34
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-04 11:32:28 +0200

    created Decoration element.

---
+++ src/components/Decoration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import DecorationImage from '../assets/Decoration.svg'
+
+function Decoration() {
+    return (
+        <img src={DecorationImage} alt="Element of site decoration."/>
+    );
+}
+
+export default Decoration;
