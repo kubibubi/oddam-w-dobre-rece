fa91ecc19fb960d8fd0004adc4b545edf419c39e
Bartosz Polanczyk <bpolanczyk@sumologic.com>
2022-04-04 02:05:58 +0200

    Pierwsze komentarze do repozytorium.

--- src/components/ButtonAction.js
+++ src/components/ButtonAction.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Dobrze przekazane props-y, dobry zalążek większego komponentu
 function ButtonAction({btnText}) {
     return (
        <button>{btnText}</button>
--- src/components/Home/HomeBanner.js
+++ src/components/Home/HomeBanner.js
@@ -3,6 +3,7 @@ import ButtonAction from '../ButtonAction';
 
 function HomeBanner() {
     return (
+        //Nie potrzebujesz tego "fragmentu" - może zostać jedynie div. Fragment przydaje się, jak chce się zgrupować elementy bez div-a (albo innego widocznego w DOM/HTML stronki elementu)
         <>
         <div className="home__banner">
             <h2>Zacznij pomagać!</h2>
