96eb7bc127d4b2a5ba85ecc09a8b00b25d9109ee
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-06 15:50:40 +0200

    created component Logout

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link as LinkRoute} from 'react-router-dom';
+
+import LoginBar from '../LoginBar';
+import NavBar from '../NavBar';
+import Decoration from '../Decoration';
+
+function Logout() {
+    return (
+        <>
+            <LoginBar/>
+            <NavBar/>
+            <section className="logout__section">
+                <h3>Wylogowanie nastąpiło</h3>
+                <h3>pomyślnie!</h3>
+                <Decoration />
+                <LinkRoute to='/'>Strona główna</LinkRoute>
+            </section>
+        </>
+    );
+}
+
+export default Logout;
