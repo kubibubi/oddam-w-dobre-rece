591a41da4d68b44782ab3c5832e6caf4fb7e644f
Jakub Jaworski <jaworski2137@gmail.com>
2022-04-06 16:29:17 +0200

    added classes to links.

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -4,8 +4,8 @@ import {Link as LinkRoute} from 'react-router-dom';
 function LoginBar() {
     return (
         <div className="login__create__wrapper">
-            <LinkRoute to='/login'>Zaloguj</LinkRoute>
-            <LinkRoute to='/register'>Załóż konto</LinkRoute>
+            <LinkRoute to='/login' className="login__link">Zaloguj</LinkRoute>
+            <LinkRoute to='/register' className="create__link">Załóż konto</LinkRoute>
         </div>
     );
 }
